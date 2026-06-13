994871d5cce2e49516b7de608b819ff7ccc438d1
Kumar Harsh <kumarharsh152005@gmail.com>
2026-06-13 16:46:51 +0530

    Fix message loading and sending

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import Messaage from "../models/message.model.js";
+import Message from "../models/message.model.js";
 
 import cloudinary from "../lib/cloudinary.js";
 import { getReceiverSocketId, io } from "../lib/socket.js";
@@ -18,20 +18,20 @@ export const getUserForSidebar = async (req, res) => {
 
 export const getMessages = async(req,res) => {
     try {
-        const { id:userToChatId } = req.params 
+        const { id: userToChatId } = req.params;
         const myId = req.user._id;
 
-        const messages = await Messages.find({
+        const messages = await Message.find({
             $or: [
-                { senderId: myId, receiverId: userToChatid },
-                { senderId: userToChatId, receiverid: myId },
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId },
             ],
-        });
+        }).sort({ createdAt: 1 });
 
-        res.status(200).json(messsages);
+        res.status(200).json(messages);
     } catch (error) {
         console.log("Error in getMessages controller: ", error.message);
-        res.status(500).json({ error: " Internal server error" });
+        res.status(500).json({ message: "Internal server error" });
     }
 };
 
@@ -65,6 +65,6 @@ export const sendMessage = async (req, res) => {
         res.status(201).json(newMessage);
     } catch (error) {
       console.log("Error in sendMessage controller: ", error.message);
-      res.status(500).json({ error: "Internal server error" });
+      res.status(500).json({ message: "Internal server error" });
     }
 };
