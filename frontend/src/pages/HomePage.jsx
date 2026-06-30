291d3ea34bc204d3a8d0f82d13ea889a27774c7c
Kumar Harsh <kumarharsh152005@gmail.com>
2026-06-30 18:29:12 +0530

    Hide mobile contacts after opening chat

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,10 +9,12 @@ const HomePage = () => {
 
   return (
     <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4">
+      <div className="flex items-center justify-center pt-20 px-2 sm:px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
-            <Sidebar />
+            <div className={`${selectedUser ? "hidden lg:flex" : "flex"} h-full`}>
+              <Sidebar />
+            </div>
 
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
           </div>
