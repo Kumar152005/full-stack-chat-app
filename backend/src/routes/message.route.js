d5b0ef2d40a4bfe4f191d9003bb8d429c6390def
Kumar Harsh <kumarharsh152005@gmail.com>
2026-07-02 20:59:01 +0530

    Route friend search APIs
    Expose protected routes for email search and adding friends.

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";  
-import { getMessages, getUserForSidebar, sendMessage } from "../controllers/message.controller.js"; 
+import {
+  addFriend,
+  getMessages,
+  getUserForSidebar,
+  searchUserByEmail,
+  sendMessage,
+} from "../controllers/message.controller.js"; 
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUserForSidebar );
+router.get("/users/search", protectRoute, searchUserByEmail);
+router.post("/users/:id/add", protectRoute, addFriend);
 router.get("/:id", protectRoute, getMessages);
 
 router.post("/send/:id", protectRoute, sendMessage);
